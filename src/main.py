6e525413c0b852243e8bfebc365ea4fd7b59201b
devSaifur <saifur2akash@gmail.com>
2024-11-05 02:00:14 +0600

    draw player

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 from constants import (
     SCREEN_HEIGHT,
@@ -11,20 +12,23 @@ from constants import (
 
 
 def main() -> None:
-    print("Hello from asteroids!")
-    print(SCREEN_WIDTH, SCREEN_HEIGHT)
-
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit frame rate to 60 fps
+        dt = clock.tick(60) // 1000
 
 
 if __name__ == "__main__":
